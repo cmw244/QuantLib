99df1b1a23eb0b66d1994db2682b5261c4ff1c73
Chris Len <clen@limebrokerage.com>
2014-11-03 20:46:08 -0500

    Moved file structure to src/ and test/folders

---
+++ src/QuantLib/Black-Scholes-Merton.cpp
@@ -0,0 +1,167 @@
+//
+//  Black-Scholes-Merton.cpp
+//  QuantLib
+//
+//  Created by Chris Len on 10/25/14.
+//  Copyright (c) 2014 Chris Len. All rights reserved.
+//
+
+#include <boost/math/distributions.hpp>
+#include "Black-Scholes-Merton.h"
+#include "Utilities.h"
+
+using namespace boost::math;
+using namespace std;
+
+// @param s: Stock price at time of valuation
+// @param k: Strike price of option being valued
+// @param r: riskless rate, assumed to be contstant over duration of option (in decimal %)
+// @param vol: volatility of underlying stock (in decimal %)
+// @param T: duration of option (in years)
+// @param dividend: Dividend payment, schedule must be set using setter method
+double BlackScholesMerton::valueCallOption(double s0, double k, double r, double vol,
+                                        double T, double dividend, vector<double> paymentSchedule)
+{
+    if(!paymentSchedule.empty()) // We need to adjust s0 for dividends
+    {
+        double adjust = 0.0;
+        
+        for(int i = 0; i < paymentSchedule.size(); i++)
+        {
+            // Discount future payment at riskless rate
+            adjust = adjust + dividend * exp(-1 * r * paymentSchedule[i]);
+        }
+        s0 = s0 - adjust;
+    }
+    double d1 = calcD1(s0,k,r,vol,T);
+    double d2 = d1 - (vol * sqrt(T)); // Optimization to avoid a couple of computations
+                                      // Equivalent to (log(s0 / k) + (r - vol*vol/2) * T) / (vol * sqrt(T))
+
+    // Valuations based off equation c = s0 * N(d1) - K * e^-rT * N(d2)
+    // where N(x) is the cumulative probability distribution for a standardized normal distribution
+    
+    return s0 * cdf(normal, d1) - k * exp(-1 * r * T)* cdf(normal, d2);
+}
+
+
+
+// Helper method that is abstracted because it can be reused (such as delta on options)
+double BlackScholesMerton::calcD1(double s0, double k, double r, double vol, double T)
+{
+    return (log(s0 / k) + (r + vol*vol/2) * T) / (vol * sqrt(T));
+}
+
+double BlackScholesMerton::valuePutOption(double s0, double k, double r, double vol,
+                                        double T, double dividend, vector<double> paymentSchedule)
+{
+    if(!paymentSchedule.empty()) // We need to adjust s0 for dividends
+    {
+        double adjust = 0.0;
+        
+        for(int i = 0; i < paymentSchedule.size(); i++)
+        {
+            // Discount future payment at riskless rate
+            adjust = adjust + dividend * exp(-1 * r * paymentSchedule[i]);
+        }
+        s0 = s0 - adjust;
+    }
+    double d1 = (log(s0 / k) + (r + vol*vol/2) * T) / (vol * sqrt(T));
+    double d2 = d1 - (vol * sqrt(T)); // Optimization to avoid a couple of computations
+    // Equivalent to (log(s0 / k) + (r - vol*vol/2) * T) / (vol * sqrt(T))
+    
+
+    // Valuations based off equation c = s0 * N(d1) - K * e^-rT * N(d2)
+    // where N(x) is the cumulative probability distribution for a standardized normal distribution
+    
+    return k * exp(-1 * r * T)* cdf(normal, -d2) - s0 * cdf(normal, -d1);
+}
+
+// Calculate value of option iteratively
+// Uses values pass into constructor to determine implied volatility
+//
+// Iterative version, utilizing the KISS approach
+
+double BlackScholesMerton::valueImpliedVolatilityIterative(std::function<double (double, double, double, double, double, double, std::vector<double>)> func,
+                                double s0, double k, double r, double T, double c)
+{
+    const double INCREMENT = 0.0001; // Go up by 1bps each iteration
+    
+    double x = 0.0; // sample vol, start at 0 and work up to 1 (100%).
+    for(int i = 0; i < MAX_ITERATIONS; i++)
+    {
+        // Try current value of volatility
+        // Note, we are not evaluating with dividends
+        double res = func(s0,k,r,x,T,0,{});
+        if(Utilities::isWithin(c,res,TOLERANCE))
+        {
+            return x;
+        }
+        // We were not close enough to actual result, try again
+        x = x + INCREMENT;
+        
+    }
+    cerr << "Implied volatility not calculated";
+    return 0.0;
+}
+
+
+// Uses bisection method to calculate implied volatility
+double BlackScholesMerton::valueImpliedVolatilityBisection(function<double (double, double, double, double, double, double, vector<double>)> func,
+                                                           double s0, double k, double r, double T, double c)
+{    
+    double high = 1.0; // Max volatility possible is 100%
+    double low = 0.0; // Min volatility possible is 0%
+    
+    double f_high,f_low,mid;
+    
+    double n = 0;
+    while(n < MAX_ITERATIONS)
+    {
+        mid = 0.5 * (high + low);
+        double res = func(s0,k,r,mid,T,0,{}) - c;
+
+        // If we get 'close enough' to a right answer
+        if(abs(res) < TOLERANCE || (0.5*(high - low) < TOLERANCE))
+        {
+            return mid;
+        }
+        n = n + 1;
+        f_high = func(s0,k,r,high,T,0,{}) - c;
+        f_low = func(s0,k,r,low,T,0,{}) - c;
+        if(f_high*res > 0) // same sign
+        {
+            high = mid;
+        }
+        else
+        {
+            low = mid;
+        }
+    }
+    cerr << "Bisection method failed";
+    return 0.0;
+}
+
+
+double BlackScholesMerton::valueImpliedVolatilityNewtonRaphson(function<double (double, double, double, double, double, double, vector<double>)> func2,
+                                                               double s0, double k, double r, double T, double c)
+{    
+    auto option = [&] (double x) -> double { return func2(s0,k,r,x,T,0,{}); };
+    auto vega = [&] (double x) -> double { return differentiate(option, x); };
+
+    // Start with volatility of 15%
+    double v = 0.15;
+    double y = func2(s0,k,r,v,T,0,{});
+    
+    while(abs(y - c) > TOLERANCE)
+    {
+        double d_x = vega(v);
+        v += (c-y)/d_x;
+        y = option(v);
+    }
+    
+    return v;
+}
+
+
+
+
---
+++ test/QuantLibUnitTests/Black-Scholes-Merton-Test.cpp
@@ -0,0 +1,395 @@
+#include <iostream>
+#include "catch.hpp"
+#include "Black-Scholes-Merton.h"
+#include "Utilities.h"
+
+TEST_CASE("Black-Scholes-Merton model")
+{
+    // Note: All result values are obtained from examples given in chapter
+    // 14 of Options, Future, and Derivitives 8th edition
+    
+    
+    //-------------------------------NO DIVIDENDS-------------------------------
+ 
+    SECTION("Value European call option without dividends")
+    {
+        double stockPrice = 42.0;
+        double strikePrice = 40.0;
+        double risklessRate = 0.1;
+        double vol = 0.2;
+        double duration = 0.5;
+        
+        double result = 4.75942; // Value is obtained from example given in chapter
+                                 // 14 of Options, Future, and Derivitives 8th edition
+        
+        double callOption = BlackScholesMerton::valueCallOption(stockPrice, strikePrice, risklessRate, vol, duration);
+        
+        REQUIRE(Utilities::closeEnough(callOption, result));
+    }
+    
+    SECTION("Value European call option without dividends")
+    {
+        double stockPrice = 49.0;
+        double strikePrice = 50.0;
+        double risklessRate = 0.05;
+        double vol = 0.2;
+        double duration = 0.3846;
+        
+        double result = 2.4005; // Value is obtained from example given in chapter 18
+                                // of Options, Future, and Derivitives 8th edition
+        
+        double callOption = BlackScholesMerton::valueCallOption(stockPrice, strikePrice, risklessRate, vol, duration);
+
+        REQUIRE(Utilities::closeEnough(callOption, result));
+    }
+    
+    SECTION("Value European put option without dividends")
+    {
+        double stockPrice = 42.0;
+        double strikePrice = 40.0;
+        double risklessRate = 0.1;
+        double vol = 0.2;
+        double duration = 0.5;
+        
+        double result = 0.808599;
+        
+        double putOption = BlackScholesMerton::valuePutOption(stockPrice, strikePrice, risklessRate, vol, duration);
+        REQUIRE(Utilities::closeEnough(putOption, result));
+    }
+    
+    //--------------------------------DIVIDENDS---------------------------------
+    
+    SECTION("Value European call option with dividends")
+    {
+        double stockPrice = 40.0;
+        double strikePrice = 40.0;
+        double risklessRate = 0.09;
+        double vol = 0.3;
+        double duration = 0.5;
+        double dividend = 0.5;
+        // Stack allocation usually faster than heap allocation
+        std::vector<double> payment{1.0/6.0,5.0/12.0};
+        
+        double result = 3.67123; // Value is obtained from example given in chapter
+                                 // 14 of Options, Future, and Derivitives 8th edition
+        
+        double callOption = BlackScholesMerton::valueCallOption(stockPrice, strikePrice,
+                                                                risklessRate, vol, duration,
+                                                                dividend, payment);
+        REQUIRE(Utilities::closeEnough(callOption, result));
+    }
+    
+    SECTION("Imlpied volatility on European call option: Iterative")
+    {
+        auto func = BlackScholesMerton::valueCallOption;
+        
+        double stockPrice = 21;
+        double strikePrice = 20;
+        double risklessRate = 0.1;
+        double duration = 0.25;
+        double optionValue = 1.875;
+        
+        double result = 0.2345;   // Value is obtained from example given in chapter
+                                  // 14 of Options, Future, and Derivitives 8th edition
+        
+        
+        // Keep track of performance to analyze vs. other methods of finding implied vol
+        auto start = std::chrono::system_clock::now();
+        double impliedVol = BlackScholesMerton::valueImpliedVolatilityIterative(func, stockPrice, strikePrice, risklessRate, duration, optionValue);
+        auto end = std::chrono::system_clock::now();
+        
+        REQUIRE(Utilities::closeEnough(impliedVol, result));
+        
+        auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
+        // convert manually because if we casted in milliseconds we would get 0 by rounding
+        std::cout << "Iterative implied vol took " <<  elapsed.count() / Utilities::NANO_TO_MILLI << " milliseconds.\n";
+    }
+    
+    
+    SECTION("Imlpied volatility on European call option: Iterative using template functions")
+    {
+        auto func = BlackScholesMerton::valueCallOption;
+        
+        double stockPrice = 21;
+        double strikePrice = 20;
+        double risklessRate = 0.1;
+        double duration = 0.25;
+        double optionValue = 1.875;
+        
+        double result = 0.2345;   // Value is obtained from example given in chapter
+        // 14 of Options, Future, and Derivitives 8th edition
+        
+        
+        // Keep track of performance to analyze vs. other methods of finding implied vol
+        auto start = std::chrono::system_clock::now();
+        double impliedVol = BlackScholesMerton::valueImpliedVolatilityIterativeTemplate(func, stockPrice, strikePrice, risklessRate, duration, optionValue);
+        auto end = std::chrono::system_clock::now();
+        
+        REQUIRE(Utilities::closeEnough(impliedVol, result));
+        
+        auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
+        // convert manually because if we casted in milliseconds we would get 0 by rounding
+        std::cout << "Iterative implied vol with templates took " <<  elapsed.count() / Utilities::NANO_TO_MILLI << " milliseconds.\n";
+    }
+    
+    SECTION("Implied volatility on European call option: Bisection method")
+    {
+        auto func = BlackScholesMerton::valueCallOption;
+        
+        double stockPrice = 21;
+        double strikePrice = 20;
+        double risklessRate = 0.1;
+        double duration = 0.25;
+        double optionValue = 1.875;
+        
+        double result = 0.23449;   // Value is obtained from example given in chapter
+                                   // 14 of Options, Future, and Derivitives 8th edition
+        
+        
+        // Keep track of performance to analyze vs. other methods of finding implied vol
+        auto start = std::chrono::system_clock::now();
+        double impliedVol = BlackScholesMerton::valueImpliedVolatilityBisection(func, stockPrice, strikePrice, risklessRate, duration, optionValue);
+        auto end = std::chrono::system_clock::now();
+        
+        REQUIRE(Utilities::closeEnough(impliedVol, result));
+        
+        auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
+        // convert manually because if we casted in milliseconds we would get 0 by rounding
+        std::cout << "Bisection method implied vol took " <<  elapsed.count() / Utilities::NANO_TO_MILLI << " milliseconds.\n";
+    }
+    
+    SECTION("Implied volatility on European call option: Bisection method")
+    {
+        auto func = BlackScholesMerton::valueCallOption;
+        
+        double stockPrice = 100;
+        double strikePrice = 100;
+        double risklessRate = 0.05;
+        double duration = 1.0;
+        double optionValue = 10.5;
+        
+        double result = 0.201355;   // Value is obtained from QuantStart
+                                   // http://www.quantstart.com/articles/Implied-Volatility-in-C-using-Template-Functions-and-Interval-Bisection
+        
+        
+        // Keep track of performance to analyze vs. other methods of finding implied vol
+        auto start = std::chrono::system_clock::now();
+        double impliedVol = BlackScholesMerton::valueImpliedVolatilityBisection(func, stockPrice, strikePrice, risklessRate, duration, optionValue);
+        auto end = std::chrono::system_clock::now();
+        
+        REQUIRE(Utilities::closeEnough(impliedVol, result));
+        
+        auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
+        // convert manually because if we casted in milliseconds we would get 0 by rounding
+        std::cout << "Bisection method implied vol took " <<  elapsed.count() / Utilities::NANO_TO_MILLI << " milliseconds.\n";
+    }
+    
+    
+    
+    SECTION("Implied volatility on European call option: Bisection method using template funtions")
+    {
+        auto func = BlackScholesMerton::valueCallOption;
+        
+        double stockPrice = 21;
+        double strikePrice = 20;
+        double risklessRate = 0.1;
+        double duration = 0.25;
+        double optionValue = 1.875;
+        
+        double result = 0.23449;   // Value is obtained from example given in chapter
+        // 14 of Options, Future, and Derivitives 8th edition
+        
+        
+        // Keep track of performance to analyze vs. other methods of finding implied vol
+        auto start = std::chrono::system_clock::now();
+        double impliedVol = BlackScholesMerton::valueImpliedVolatilityBisectionTemplate(func, stockPrice, strikePrice, risklessRate, duration, optionValue);
+        auto end = std::chrono::system_clock::now();
+        
+        REQUIRE(Utilities::closeEnough(impliedVol, result));
+        
+        auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
+        // convert manually because if we casted in milliseconds we would get 0 by rounding
+        std::cout << "Bisection method implied vol with templates took " <<  elapsed.count() / Utilities::NANO_TO_MILLI << " milliseconds.\n";
+    }
+    
+    SECTION("Implied volatility on European call option: Bisection method using template functions")
+    {
+        auto func = BlackScholesMerton::valueCallOption;
+        
+        double stockPrice = 100;
+        double strikePrice = 100;
+        double risklessRate = 0.05;
+        double duration = 1.0;
+        double optionValue = 10.5;
+        
+        double result = 0.201355;   // Value is obtained from QuantStart
+        // http://www.quantstart.com/articles/Implied-Volatility-in-C-using-Template-Functions-and-Interval-Bisection
+        
+        
+        // Keep track of performance to analyze vs. other methods of finding implied vol
+        auto start = std::chrono::system_clock::now();
+        double impliedVol = BlackScholesMerton::valueImpliedVolatilityBisectionTemplate(func, stockPrice, strikePrice, risklessRate, duration, optionValue);
+        auto end = std::chrono::system_clock::now();
+        
+        REQUIRE(Utilities::closeEnough(impliedVol, result));
+        
+        auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
+        // convert manually because if we casted in milliseconds we would get 0 by rounding
+        std::cout << "Bisection method implied vol with templates took " <<  elapsed.count() / Utilities::NANO_TO_MILLI << " milliseconds.\n";
+    }
+    
+    SECTION("Implied volatility on European call option: Newton-Raphson method")
+    {
+        auto func = BlackScholesMerton::valueCallOption;
+        
+        double stockPrice = 21;
+        double strikePrice = 20;
+        double risklessRate = 0.1;
+        double duration = 0.25;
+        double optionValue = 1.875;
+        
+        double result = 0.23449;   // Value is obtained from example given in chapter
+                                   // 14 of Options, Future, and Derivitives 8th edition
+        
+        
+        // Keep track of performance to analyze vs. other methods of finding implied vol
+        auto start = std::chrono::system_clock::now();
+        double impliedVol = BlackScholesMerton::valueImpliedVolatilityNewtonRaphson(func, stockPrice, strikePrice, risklessRate, duration, optionValue);
+        auto end = std::chrono::system_clock::now();
+        
+        //REQUIRE(Utilities::closeEnough(impliedVol, result));
+        
+        auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
+        // convert manually because if we casted in milliseconds we would get 0 by rounding
+        std::cout << "Newton-Raphson method implied vol took " <<  elapsed.count() / Utilities::NANO_TO_MILLI << " milliseconds.\n";
+    }
+    
+    SECTION("Implied volatility on European call option: Newton-Raphson method")
+    {
+        auto func = BlackScholesMerton::valueCallOption;
+        
+        double stockPrice = 100;
+        double strikePrice = 100;
+        double risklessRate = 0.05;
+        double duration = 1.0;
+        double optionValue = 10.5;
+        
+        double result = 0.201314;   // Value is obtained from QuantStart
+                                    // http://www.quantstart.com/articles/Implied-Volatility-in-C-using-Template-Functions-and-Interval-Bisection
+        
+        
+        // Keep track of performance to analyze vs. other methods of finding implied vol
+        auto start = std::chrono::system_clock::now();
+        double impliedVol = BlackScholesMerton::valueImpliedVolatilityNewtonRaphson(func, stockPrice, strikePrice, risklessRate, duration, optionValue);
+        auto end = std::chrono::system_clock::now();
+        
+        REQUIRE(Utilities::closeEnough(impliedVol, result));
+        
+        auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
+        // convert manually because if we casted in milliseconds we would get 0 by rounding
+        std::cout << "Newton-Raphson method implied vol took " <<  elapsed.count() / Utilities::NANO_TO_MILLI << " milliseconds.\n";
+    }
+    
+    SECTION("Implied volatility on European call option: Newton-Raphson method using template functions")
+    {
+        auto func = BlackScholesMerton::valueCallOption;
+        
+        double stockPrice = 21;
+        double strikePrice = 20;
+        double risklessRate = 0.1;
+        double duration = 0.25;
+        double optionValue = 1.875;
+        
+        double result = 0.23449;   // Value is obtained from example given in chapter
+        // 14 of Options, Future, and Derivitives 8th edition
+        
+        
+        // Keep track of performance to analyze vs. other methods of finding implied vol
+        auto start = std::chrono::system_clock::now();
+        double impliedVol = BlackScholesMerton::valueImpliedVolatilityNewtonRaphsonTemplate(func, stockPrice, strikePrice, risklessRate, duration, optionValue);
+        auto end = std::chrono::system_clock::now();
+        
+        REQUIRE(Utilities::closeEnough(impliedVol, result));
+        
+        auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
+        // convert manually because if we casted in milliseconds we would get 0 by rounding
+        std::cout << "Newton-Raphson method implied vol with templates took " <<  elapsed.count() / Utilities::NANO_TO_MILLI << " milliseconds.\n";
+    }
+    
+    SECTION("Implied volatility on European call option: Newton-Raphson method using template functions")
+    {
+        auto func = BlackScholesMerton::valueCallOption;
+        
+        double stockPrice = 100;
+        double strikePrice = 100;
+        double risklessRate = 0.05;
+        double duration = 1.0;
+        double optionValue = 10.5;
+        
+        double result = 0.201314;   // Value is obtained from QuantStart
+        // http://www.quantstart.com/articles/Implied-Volatility-in-C-using-Template-Functions-and-Interval-Bisection
+        
+        
+        // Keep track of performance to analyze vs. other methods of finding implied vol
+        auto start = std::chrono::system_clock::now();
+        double impliedVol = BlackScholesMerton::valueImpliedVolatilityNewtonRaphsonTemplate(func, stockPrice, strikePrice, risklessRate, duration, optionValue);
+        auto end = std::chrono::system_clock::now();
+        
+        REQUIRE(Utilities::closeEnough(impliedVol, result));
+        
+        auto elapsed = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
+        // convert manually because if we casted in milliseconds we would get 0 by rounding
+        std::cout << "Newton-Raphson method implied vol with templates took " <<  elapsed.count() / Utilities::NANO_TO_MILLI << " milliseconds.\n";
+    }
+    
+    SECTION("D1 helper method of Black-Scholes-Merton formula")
+    {
+        double stockPrice = 42.0;
+        double strikePrice = 40.0;
+        double risklessRate = 0.1;
+        double vol = 0.2;
+        double duration = 0.5;
+        
+        double result = 0.7693; // Value is obtained from example given in chapter
+        // 14 of Options, Future, and Derivitives 8th edition
+        
+        double d1 = BlackScholesMerton::calcD1(stockPrice, strikePrice, risklessRate, vol, duration);
+        
+        REQUIRE(Utilities::closeEnough(d1, result));
+    }
+    
+    SECTION("D1 helper method of Black-Scholes-Merton formula")
+    {
+        double stockPrice = 49.0;
+        double strikePrice = 50.0;
+        double risklessRate = 0.05;
+        double vol = 0.2;
+        double duration = 0.3846;
+        
+        double result = 0.054173; // Value is obtained from example given in chapter 18
+                                // of Options, Future, and Derivitives 8th edition
+        
+        double d1 = BlackScholesMerton::calcD1(stockPrice, strikePrice, risklessRate, vol, duration);
+        
+        REQUIRE(Utilities::closeEnough(d1, result));
+    }
+    
+    SECTION("Differentiate function y=x^2")
+    {
+        // y = x^2
+        auto func = [&](double x) -> double { return x * x; };
+        
+        
+        double res = differentiate(func, 2);
+        REQUIRE(Utilities::closeEnough(res, 4.0));
+        
+        res = differentiate(func, 1);
+        REQUIRE(Utilities::closeEnough(res, 2.0));
+        
+        res = differentiate(func, -2);
+        REQUIRE(Utilities::closeEnough(res, -4.0));
+        
+        res = differentiate(func, 8);
+        REQUIRE(Utilities::closeEnough(res, 16.0));
+    }
+}
